22be2d6229b0c7ef4e07c2596d6bf08c3fd635bd
Will <wrightw404@gmail.com>
2022-11-02 00:17:14 -0700

    added aboutme section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ImageOne from "./components/imageOne.js";
 import ImageTwo from "./components/imageTwo.js";
+import AboutSection from "./components/aboutSection.js";
 
 
 function App() {
   return (
     <div>
      <ImageOne/>
+     <AboutSection/>
      <ImageTwo/>
      <ImageTwo/>
      <ImageOne/>
