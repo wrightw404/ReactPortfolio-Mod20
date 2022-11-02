712eb24f9fbeaa6d36c630ab92516424db30ea77
Will <wrightw404@gmail.com>
2022-11-01 23:10:25 -0700

    styled first image in parallax

--- src/components/imageOne.js
+++ src/components/imageOne.js
@@ -3,10 +3,12 @@ import Sunset from '../img/mainBackground.jpg'
 import { Parallax } from 'react-parallax';
 
 const ImageOne = () => (
-    <Parallax bgImage={ Sunset } bgImageAlt="la jolla cove" strength={800}>
+    <Parallax className='image' bgImage={ Sunset } bgImageAlt="la jolla cove" strength={800}>
        <div className='content'>
-            <span className='img-text'>Welcome Bill</span> 
+            <span className='img-text'><h1>William W. Wright</h1></span> 
+            <h4>Experience Architect // Web Developer</h4>
        </div>
+       
     </Parallax>
 );
 
