3233684884916b57b644c5d86548c68b22c544d4
Will <wrightw404@gmail.com>
2022-11-02 22:08:29 -0700

    fixed hover issue in projects

--- src/components/sections/projectSection.js
+++ src/components/sections/projectSection.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import voting from '../../assets/img/votingProject.jpeg';
 import fuel from '../../assets/img/fuellogo.png';
 import btat from '../../assets/img/beenthereatethat.png';
 import style from '../../assets/img/styleSheet.jpg';
@@ -17,7 +16,7 @@ const ProjectSection = () => {
                     
                     <a href="https://drive.google.com/file/d/1zrHqnVYQwnl957N1oOgftZMymtRaFybG/preview" target="_blank" rel="noreferrer"> <img className="popup" src={noSQL} alt="voter registration project"></img> </a>
                     <p className="summary">Check out my social network web application
-                    <a className="repo" href='https://github.com/wrightw404/socialNetwork-nosql'>GitHub Repository</a>
+                    <a className="repo" href='https://github.com/wrightw404/socialNetwork-nosql' target="_blank" rel="noreferrer">GitHub Repository</a>
                     </p>
                         
                     
@@ -34,7 +33,7 @@ const ProjectSection = () => {
                     <a href="https://beenthereatethat.herokuapp.com/" target="_blank" rel="noreferrer"><img className="popup" src={btat}alt="Been There Ate That">
                         </img></a>
                         <p className="summary">Been There Ate That!
-                        <a className="repo" href='https://github.com/ddangeles/BeenThere-AteThat'>GitHub Repository</a>
+                        <a className="repo" href='https://github.com/ddangeles/BeenThere-AteThat' target="_blank" rel="noreferrer">GitHub Repository</a>
                         </p>
                     
                   <a href={style} rel="noreferrer" target="_blank"><img className="popup" src={style} alt="portfolio project"></img></a>
