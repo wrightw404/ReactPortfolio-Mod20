96640bcd1ed76bf7d40a0fbaf319a29372010107
Will <wrightw404@gmail.com>
2022-11-02 22:01:37 -0700

    fixed submit form format

--- src/components/parallaxImg/ImageFive.js
+++ src/components/parallaxImg/ImageFive.js
@@ -4,7 +4,7 @@ import { Parallax } from 'react-parallax';
 
 const ImageFive = () => (
     <Parallax className='image image-five' bgImage={ Sunset } bgImageAlt="la jolla cove" strength={800}>
-       	<div className="ptext">
+       	<div className="ptext form-title">
 		    <span className="border">
 			    <div className="statement">
 	                <p>Please reach out to me if you have any questions</p>
@@ -13,16 +13,19 @@ const ImageFive = () => (
         </div>
 				
         <div className="contact-form">
-		    <form id="contact-form" method="post" action="mailto:wrightw404@gmail.com" enctype="text/plain">
+
+			
+		    <form id="contact-form" method="post" action="mailto:wrightw404@gmail.com" encType="text/plain">
 			
 			<input name="name" type="text" class="form-control" placeholder="Your Name" required></input>	
 			
             <input name="email" type="email" class="form-control" placeholder="Your Email" required></input>
-			
-            <input name="message" class="form-control" placeholder="Message" rows="6" required></input>
-			
-			<input type="submit" class="form-control submit" value="SEND MESSAGE"></input>
 		
+            <input name="message" class="form-control" placeholder="Message" rows="6" required></input>
+		
+			<input type="submit" class="form-control submit" value="SEND MESSAGE"></input>
+			
+
             </form>
     
 	    </div>
